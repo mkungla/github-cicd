7f1d3825a5fe8f45b5b753d259d1fcb4217ddc3a
Marko Kungla <marko.kungla@gmail.com>
2021-08-22 17:38:00 +0300

    add readme update script

---
+++ .github/actions-scripts/update-readme.js
@@ -0,0 +1,60 @@
+import * as core from '@actions/core'
+import { exec } from '@actions/exec'
+import * as github from '@actions/github'
+
+export async function git(...args) {
+  core.debug(`Executing Git: ${args.join(' ')}`);
+  const userArgs = [
+      '-c',
+      'user.name=github-action-readme',
+      '-c',
+      'user.email=github@users.noreply.github.com',
+      '-c',
+      'http.https://github.com/.extraheader=', // This config is necessary to support actions/checkout@v2 (#9)
+  ];
+  const res = await capture('git', userArgs.concat(args));
+  if (res.code !== 0) {
+    throw new Error(`Command 'git ${args.join(' ')}' failed: ${JSON.stringify(res)}`);
+  }
+  return res.stdout;
+}
+
+function getRemoteUrl() {
+  /* eslint-disable @typescript-eslint/camelcase */
+  const fullName = github.context.payload.repository?.full_name
+  /* eslint-enable @typescript-eslint/camelcase */
+
+  if (!fullName) {
+      throw new Error(`Repository info is not available in payload: ${JSON.stringify(github.context.payload)}`)
+  }
+
+  return `https://x-access-token:${core.getInput('github-token')}@github.com/${fullName}.git`
+}
+
+export async function gitpush() {
+  core.debug('Executing git push')
+
+  const remote = getRemoteUrl()
+  let args = ['push', remote, `main:main`, '--no-verify'];
+  if (options.length > 0) {
+      args = args.concat(options);
+  }
+
+  return cmd(...args);
+}
+
+async function main() {
+  await git('add', '-A')
+  await git('commit', '-m', `generate and update (README.md)`);
+
+  try {
+    await gitpush()
+    core.debug('git push is done')
+  } catch (err) {
+    core.warning('Auto-push failed')
+    core.error(err)
+  }
+}
+
+
+main().catch(e => core.setFailed(e.message))
